f1c18995c7ac50c862ce81ae7390d8d175809e35
Stegnerd <Stegnerd@uindy.edu>
2015-08-09 16:32:24 -0400

    Import ADAM2 Project
    This is the first uploading if the antiDrug project.

---
+++ Assets/ADAM2/Scripts/TextBox.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TextBox : MonoBehaviour {
+    void onCollisionEnter(Collider other)
+    {
+        if(other.gameObject.tag=="Player")
+        {
+            GUI.Label(new Rect(10, 10, 50, 50), "DRUGSSSS!");
+            Debug.Log("Touching ME");
+        }
+
+    }
+}
